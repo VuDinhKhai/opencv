b2a10db64f63d661bad1079e125999655ea18b38
Vu Dinh Khai <Khaidinhvu2610@gmail.com>
2021-10-29 16:30:19 +0700

    add opencv.py

--- opencv.py
+++ opencv.py
@@ -5,7 +5,7 @@ i=0
 start_time=time.time()
 for i in range(50):
     return_value, image = cap.read()
-    cv2.imwrite('C:/Users/DELL/OneDrive/Máy tính/Webcam/anh_thu_'+str(i)+'.png',image)
+    cv2.imwrite('D:/Webcam/anh_thu_'+str(i)+'.jpg',image)
 end_time=time.time()
 print(end_time-start_time)
 cap.release() 
